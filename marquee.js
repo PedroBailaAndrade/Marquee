90583fa95f1154fc65bb149e75709466468f3f77
PedroBailaAndrade <52913878+PedroBailaAndrade@users.noreply.github.com>
2021-04-29 10:24:51 +0100

    WIP

---
+++ marquee.js
@@ -0,0 +1,29 @@
+// Marquee 
+
+x = document.getElementsByClassName("marqueeContent");
+		console.log(x.item(0));
+
+	// const marqueeWindow = ;
+	function marqueeContent() {
+		
+	}
+
+	// const marqueeContentChild = ;
+
+	// function moveMarqueeContent() {
+	// 	console.log(marqueeContent());
+	// }
+
+	// moveMarqueeContent();
+
+	// function animateMarqueeContent() {
+	//   console.log(moveMarqueeContent());
+	//   requestAnimationFrame(animateMarqueeContent);
+	// }
+	// requestAnimationFrame(animateMarqueeContent);
+
+	// function calculateMarqueWidth
+	// function selectMarqueChildren
+	// function calculateMarqueChildrenWidth 	
+	// function moveVerticalMarqueContent 	
+	// function repeatMarqueChildren
