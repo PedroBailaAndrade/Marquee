8b84e1f6c81c5aeaca139fcbbc74ba443fb87452
PedroBailaAndrade <52913878+PedroBailaAndrade@users.noreply.github.com>
2021-05-06 11:06:23 +0100

    WIP

--- marquee.js
+++ marquee.js
@@ -1,29 +1,64 @@
 // Marquee 
 
-x = document.getElementsByClassName("marqueeContent");
-		console.log(x.item(0));
+	const px = "px";
 
-	// const marqueeWindow = ;
-	function marqueeContent() {
-		
+	const [marqueeWindow] = document.getElementsByClassName("marqueeWindow");
+	// console.log(marqueeWindow);
+
+	const marqueeWindowWidth = marqueeWindow.offsetWidth;
+
+	let [marqueeContent] = document.getElementsByClassName("marqueeContent");
+	const marqueeContentWidth = marqueeContent.offsetWidth;
+
+	// console.log(marqueeContent)
+
+	copyMarqueeContent();
+
+	function copyMarqueeContent() {
+		const timesToCopy =  Math.round(marqueeWindowWidth / marqueeContentWidth) + 1;
+		// console.log("times to compy: ", timesToCopy);
+		for (let i = 1; i <= timesToCopy; i++) {
+			marqueeContentClone = marqueeContent.cloneNode(true);
+			// console.log('content copied');
+			// console.log(marqueeContentClone);
+			marqueeWindow.appendChild(marqueeContentClone);
+			}
+	}
+	
+	let marqueeCollection = document.getElementsByClassName("marqueeContent")
+	// console.log(marqueeCollection);
+
+	let marqueeCollectionPositions = [];
+
+	function printMarqueeCollectionPositions(array) {
+		for (let index = 0; index < array.length; index++) {
+			// console.log(array[index].offsetLeft);
+			marqueeCollectionPositions.push(array[index].offsetLeft);
+		}
+		// console.log(marqueeCollectionPositions);
 	}
 
-	// const marqueeContentChild = ;
+	printMarqueeCollectionPositions(marqueeCollection);
+	
+	function moveMarqueeCollectionPositions() {
 
-	// function moveMarqueeContent() {
-	// 	console.log(marqueeContent());
-	// }
+		const marqueeLastContentPosition = marqueeCollection[marqueeCollection.length - 1].offsetLeft + px;
+		// console.log(marqueeLastContentPosition);
 
-	// moveMarqueeContent();
+		for (let index = 0; index < marqueeCollectionPositions.length; index++) {
+			marqueeCollection[index].style.left = marqueeCollectionPositions[index] + px;
+			marqueeCollection[index].style.position = "absolute";
 
-	// function animateMarqueeContent() {
-	//   console.log(moveMarqueeContent());
-	//   requestAnimationFrame(animateMarqueeContent);
-	// }
-	// requestAnimationFrame(animateMarqueeContent);
+			setInterval(function(){ 
+				if (marqueeCollection[index].offsetLeft == - marqueeContentWidth) {
+					// console.log("stop");
+					marqueeCollection[index].style.left = marqueeLastContentPosition;
+				}
+				marqueeCollection[index].style.left = (marqueeCollection[index].style.left.replace("px", "") - 1) + px;
+			}, 10);
 
-	// function calculateMarqueWidth
-	// function selectMarqueChildren
-	// function calculateMarqueChildrenWidth 	
-	// function moveVerticalMarqueContent 	
-	// function repeatMarqueChildren
+		}
+
+	}
+
+	moveMarqueeCollectionPositions();
